942161929106cb527072d755e2de6e5ed76cfca7
unknown <374688995@qq.com>
2023-03-05 17:54:55 +0800

    更改输出文件位置

---
+++ dist/assets/mermaid-mindmap.esm.min-1f2f7050.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-80fc4c1d.js";import"./framework-13182686.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
